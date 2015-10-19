9ad7b4118deb70e95d881711ee296d300c52552d
smiley <smiley@chillerlan.net>
2015-10-19 16:44:51 +0200

    initial commit

---
+++ src/Modules/Html5/Video.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * Class Video
+ *
+ * @filesource   Video.php
+ * @created      12.10.2015
+ * @package      chillerlan\bbcode\Modules\Html5
+ * @author       Smiley <smiley@chillerlan.net>
+ * @copyright    2015 Smiley
+ * @license      MIT
+ */
+
+namespace chillerlan\bbcode\Modules\Html5;
+
+use chillerlan\bbcode\BBTemp;
+use chillerlan\bbcode\Modules\ModuleInterface;
+use chillerlan\bbcode\Modules\Html5\Html5BaseModule;
+
+/**
+ * @todo
+ */
+class Video extends Html5BaseModule implements ModuleInterface{
+
+	/**
+	 * @var array
+	 */
+	protected $tags = ['video', 'dmotion', 'vimeo', 'youtube', 'moddb'];
+
+	/**
+	 * @var
+	 */
+	private $_host;
+
+	/**
+	 * @var
+	 */
+	private $_url;
+
+	/**
+	 * @var
+	 */
+	private $_video_url;
+
+	/**
+	 * @var
+	 */
+	private $_flash;
+
+	/**
+	 * Returns the processed bbcode
+	 *
+	 * @return string a HTML snippet
+	 */
+	public function transform(){
+		$this->check_tag();
+
+		$this->_flash = $this->get_attribute('flash');
+
+		$provider = $this->_get_provider();
+		call_user_func([$this, $provider]);
+
+		if($provider === 'video'){
+			return '<video src="'.$this->_video_url.'"'.$this->get_css_class('bb-video').' preload="auto" controls="true"></video>';
+		}
+		else{
+			$object = '<iframe src="'.$this->_video_url.'" allowfullscreen></iframe>';
+
+			if($this->_flash){
+				$object = '<object type="application/x-shockwave-flash" data="'.$this->_video_url.'">'
+						.'<param name="allowfullscreen" value="true">'
+						.'<param name="wmode" value="opaque" />'
+						.'<param name="movie" value="'.$this->_video_url.'" />'.
+						'</object>';
+			}
+
+			return '<div'.$this->get_css_class('bb-video').'>'.$object.'</div>';
+		}
+	}
+
+	/**
+	 * @return string
+	 */
+	private function _get_provider(){
+		$this->_url = parse_url($this->content);
+		$this->_host = isset($this->_url['host']) ? str_replace('www.', '', $this->_url['host']) : false;
+		$_bbtag = $this->bbtag();
+
+		switch(true){
+			case $this->tag === 'vimeo' || $_bbtag === 'vimeo' || $this->_host === 'vimeo.com':
+				return 'vimeo';
+			case $this->tag === 'youtube' || $_bbtag === 'youtube' || in_array($this->_host, ['youtube.com', 'youtu.be']):
+				return 'youtube';
+			case $this->tag === 'moddb' || $_bbtag === 'moddb' || $this->_host === 'moddb.com':
+				return 'moddb';
+			case $this->tag === 'dmotion' || $_bbtag === 'dmotion' || in_array($this->_host, ['dailymotion.com', 'dai.ly']):
+				return 'dmotion';
+			default: return 'video';
+		}
+	}
+
+	/**
+	 *
+	 */
+	private function dmotion(){
+		if($this->_host === 'dailymotion.com'){
+			$x = explode('_', str_replace('/video/', '', $this->_url['path']), 2);
+			$id = $x[0];
+		}
+		else if($this->_host === 'dai.ly'){
+			$id = $this->_url['path'];
+		}
+		else{
+			$id = $this->content;
+		}
+		$this->_video_url = 'http://www.dailymotion.com/'.($this->_flash ? 'swf' : 'embed').'/video/'.preg_replace('#[^a-z\d]#i', '', $id);
+	}
+
+	/**
+	 *
+	 */
+	private function moddb(){
+		if($this->_host === 'moddb.com' && strpos('http://www.moddb.com/media/', $this->content) === 0){
+			$id = $this->_url['path'];
+		}
+		else{
+			$id = $this->content;
+		}
+		$this->_video_url = 'http://www.moddb.com/media/'.($this->_flash ? 'embed/' : 'iframe/').preg_replace('/[^\d]/', '', $id);
+	}
+
+	/**
+	 *
+	 */
+	private function video(){
+		// todo: check video...
+		$this->_video_url = $this->content;
+	}
+
+	/**
+	 *
+	 */
+	private function vimeo(){
+		// since the video id is the only numeric part in a common vimeo share url, we can safely strip anything which is not number
+		$this->_video_url = 'https://'.($this->_flash ? 'vimeo.com/moogaloop.swf?clip_id=' : 'player.vimeo.com/video/').preg_replace('/[^\d]/', '', $this->content);
+	}
+
+	/**
+	 *
+	 */
+	private function youtube(){
+		if($this->_host === 'youtube.com'){
+			parse_str($this->_url['query'], $q);
+			$id = $q['v'];
+		}
+		else if($this->_host === 'youtu.be'){
+			$id = $this->_url['path'];
+		}
+		else{
+			// check if needed: http://gdata.youtube.com/feeds/api/videos/[VIDEO_ID]
+			$id = $this->content;
+		}
+		// clean out any suspect characters -> #[^a-z\d-_]#i
+		$this->_video_url = 'https://www.youtube.com/'.($this->_flash ? 'v/' : 'embed/').preg_replace('/[^a-z\d-_]/i', '', $id);
+	}
+
+}
