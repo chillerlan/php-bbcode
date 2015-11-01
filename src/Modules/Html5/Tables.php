7d5fd7c7af52ef1037c794d75557beff63ba2085
smiley <smiley@chillerlan.net>
2015-11-01 07:25:32 +0100

    finally! :octocat:

--- src/Modules/Html5/Tables.php
+++ src/Modules/Html5/Tables.php
@@ -17,13 +17,13 @@ use chillerlan\bbcode\Modules\ModuleInterface;
 use chillerlan\bbcode\Modules\Html5\Html5BaseModule;
 
 /**
- * @todo work in progress, strange bug with singletag regex and [col]
+ * Tables, as HTML5 as possible...
+ *
  * @link http://www.w3.org/TR/html5/tabular-data.html
  */
 class Tables extends Html5BaseModule implements ModuleInterface{
 
 	/**
-	 *
 	 * @var array
 	 */
 	protected $tags = ['col', 'colgroup', 'caption', 'table', 'thead', 'tbody', 'tfoot', 'tr', 'td', 'th'];
@@ -38,16 +38,8 @@ class Tables extends Html5BaseModule implements ModuleInterface{
 	 *
 	 * @return string a HTML snippet
 	 */
-	public function transform(){
-		$this->check_tag();
-
+	public function _transform(){
 		switch(true){
-			case 'co':
-				$span = $this->get_attribute('span');
-				$span = $span ? ' span="'.$span.'"' : '';
-				return '<col'.$span.$this->get_css_class().' />';
-			case $this->tag_in(['colgroup']):
-				return $this->colgroup();
 			case $this->tag_in(['tr', 'thead', 'tbody', 'tfoot']):
 				return $this->rows();
 			case $this->tag_in(['td', 'th']):
@@ -61,53 +53,82 @@ class Tables extends Html5BaseModule implements ModuleInterface{
 	 * @return string
 	 */
 	private function table(){
+		if(empty($this->content)){
+			return '';
+		}
 
+		$this->_style = [
+			'width' => $this->get_attribute('width'),
+		];
 
-
-		return '<table class="bb-table" style="'.(isset($this->attributes['width']) ? 'width:'.$this->attributes['width'].';' : '').'">'.$this->content.'</table>';
-
+		return '<table'.$this->get_css_class('bb-table').$this->get_style().'>'.$this->eol($this->content).'</table>';
 	}
 
 	/**
 	 * @return string
 	 */
-	private function caption(){
+	private function col(){
+		$span = $this->get_attribute('span');
 
-		return '<caption>'.$this->content.'</caption>';
-
+		return '<col'.($span ? ' span="'.intval($span).'"' : '').$this->get_css_class().' />';
 	}
 
 	/**
 	 * @return string
 	 */
 	private function colgroup(){
-		return '<'.$this->tag.$this->get_attribute('span', '').$this->get_css_class().($this->tag === 'colgroup' ? '></colgroup>' : ' />');
+		$span = $this->get_attribute('span');
+
+		return '<colgroup'.($span ? ' span="'.intval($span).'"' : '').$this->get_css_class().'>'.$this->eol($this->content).'</colgroup>';
 	}
 
+	/**
+	 * @return string
+	 */
+	private function caption(){
+		return '<caption>'.$this->eol($this->content, $this->eol_token).'</caption>';
+	}
 
 	/**
 	 * @return string
 	 */
 	private function rows(){
-		// attribute align is invalid for XHTML1.1 and HTML5 - solved by css
-		return str_replace(["\r", "\n"], '', '<'.$this->tag.' style="'.
-				(isset($this->attributes['align']) && in_array(strtolower($this->attributes['align']), ['left', 'right', 'center']) ? 'text-align:'.$this->attributes['align'].';' : 'text-align:left;').
-				'">'.$this->content.'</'.$this->tag.'>');
+		return '<'.$this->tag.'>'.$this->eol($this->content).'</'.$this->tag.'>';
 	}
 
 	/**
 	 * @return string
 	 */
 	private function cells(){
-		// todo: valign
-		// attribute align is invalid for XHTML1.1 and HTML5 - solved by css
-		// nowrap and width attributes are invalid for xhtml1.1 - solved as css
-		return '<'.$this->tag.
-		(isset($this->attributes['colspan']) && is_numeric($this->attributes['colspan']) ? ' colspan="'.ceil($this->attributes['colspan']).'"' : '').' style="'.
-		(array_key_exists('nowrap', $this->attributes) ? 'white-space:nowrap;' : '').
-		(isset($this->attributes['width']) ? 'width:'.$this->attributes['width'].';' : '').
-		(isset($this->attributes['align']) && in_array(strtolower($this->attributes['align']), ['left', 'right', 'center']) ? 'text-align:'.$this->attributes['align'].';' : 'text-align:left;').
-		'">'.nl2br($this->content).'</'.$this->tag.'>'; //,1
+
+		switch(true){
+			case $align = $this->get_attribute('align'):
+				$this->_style['text-align'] = in_array($align, $this->_text_align) ? $align : 'inherit';
+				break;
+			case $valign = $this->get_attribute('valign'):
+				$this->_style['vertical-align'] = in_array($valign, ['baseline', 'bottom', 'middle', 'top']) ? $valign : 'inherit';
+				break;
+			case $width = $this->get_attribute('width'):
+				$this->_style['width'] = $width;
+				break;
+			case $this->get_attribute('nowrap'):
+				$this->_style['white-space'] = 'nowrap';
+				break;
+		}
+
+		$span = '';
+		foreach(['colspan', 'rowspan'] as $s){
+			$_span = $this->get_attribute($s);
+			$span .=  $_span ? ' '.$s.'="'.intval($_span).'"' : '';
+		}
+
+		$abbr  = '';
+		if($this->tag === 'th'){
+			$_abbr = $this->get_attribute('abbr');
+			$abbr = $_abbr ? ' abbr="'.$_abbr.'"' : '';
+		}
+
+		return '<'.$this->tag.$span.$abbr.$this->get_style().'>'.$this->eol($this->content, $this->eol_token).'</'.$this->tag.'>';
 	}
 
 }
