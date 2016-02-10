8152e86cb680636510090c32c487457053b3e7bd
smiley <smiley@chillerlan.net>
2016-02-10 01:16:21 +0100

    i have no idea what exceptions this will cause on travis... :suspect:

---
+++ tests/ParserPregJitTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *
+ * @filesource   ParserPregJitTest.php
+ * @created      09.02.2016
+ * @package      chillerlan\BBCodeTest
+ * @author       Smiley <smiley@chillerlan.net>
+ * @copyright    2015 Smiley
+ * @license      MIT
+ */
+
+namespace chillerlan\BBCodeTest;
+
+use chillerlan\bbcode\Parser;
+
+class ParserPregJitTest extends \PHPUnit_Framework_TestCase{
+
+	public function testPregError(){
+		$bbcode = file_get_contents(dirname(__FILE__).'/bbcode_preg_error.txt');
+		(new Parser)->parse($bbcode);
+	}
+
+}
