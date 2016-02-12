363e3b1414c1ebafda8137101c12e9ccb3c64541
smiley <smiley@chillerlan.net>
2016-02-12 13:29:40 +0100

    added magic

--- src/Language/LanguageBase.php
+++ src/Language/LanguageBase.php
@@ -12,12 +12,42 @@
 
 namespace chillerlan\bbcode\Language;
 
+use chillerlan\bbcode\Traits\ClassLoaderTrait;
+
 /**
  *
  */
 class LanguageBase{
+	use ClassLoaderTrait;
 
+	/**
+	 * It's magic.
+	 *
+	 * @param $name
+	 * @param $arguments
+	 *
+	 * @return mixed
+	 */
 	public function __call($name, $arguments){
-		// TODO: Implement __call() method.
+		return $this->string($name, ...$arguments);
 	}
+
+	/**
+	 * Returns a language string for a given key and overrides the current language if desired.
+	 *
+	 * @param      $key
+	 * @param null $override_language
+	 *
+	 * @return mixed
+	 * @throws \chillerlan\bbcode\BBCodeException
+	 */
+	public function string($key, $override_language = null){
+
+		if($override_language){
+			return $this->__loadClass($override_language, LanguageInterface::class)->{$key}();
+		}
+
+		return $this->{$key};
+	}
+
 }
